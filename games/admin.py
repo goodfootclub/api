0b90a81b7a0118640999417322f044ed214d856b
Ignat Shining <mail@igonato.com>
2016-11-15 15:32:15 +0000

    Fix opnlayers api link to use https

--- games/admin.py
+++ games/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GeoModelAdmin
 
 from .models import Game, Location, RsvpStatus
 
 
+class LocationAdmin(GeoModelAdmin):
+    openlayers_url = 'https://openlayers.org/api/2.13.1/OpenLayers.js'
+
 admin.site.register(Game)
-admin.site.register(Location)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(RsvpStatus)
