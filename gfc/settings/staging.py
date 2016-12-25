0af0712f88efc5d5ec5c3ce513b1e30bcb5230ad
Ignat Shining <mail@igonato.com>
2016-12-26 01:45:42 +0300

    Staging settings

---
+++ gfc/settings/staging.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+
+globals().update(vars(sys.modules['gfc.settings']))
+
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ADMINS = [('Ignat', 'mail@igonato.com')]
+
+ALLOWED_HOSTS = ['dev.goodfoot.club']
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_PASS')
+EMAIL_PORT = 587
