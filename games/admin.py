dd9b6e0727dbf4b40830f48b1720193df11d893d
Ignat Shining <mail@igonato.com>
2017-04-17 20:30:05 +0100

    Maps with streets on them in the admin interface

--- games/admin.py
+++ games/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.contrib.gis.admin import GeoModelAdmin
+from django.contrib.gis.admin import OSMGeoAdmin
 
 from .models import Game, Location, RsvpStatus
 
 
-class LocationAdmin(GeoModelAdmin):
+class LocationAdmin(OSMGeoAdmin):
     openlayers_url = 'https://openlayers.org/api/2.13.1/OpenLayers.js'
 
 admin.site.register(Game)
