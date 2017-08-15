2899b2ca684b12d871283a9706b2a2a1baaf21ad
Ignat Shining <mail@igonato.com>
2017-08-15 11:33:31 +0300

    Remove viewset docstring magic. Use coreapi schema

--- main/viewsets.py
+++ main/viewsets.py
@@ -1,57 +1,19 @@
-from functools import wraps
-from types import FunctionType
-
 from rest_framework.viewsets import ModelViewSet
 
 
-class ActionDocViewsetMeta(type):
+class AppViewSet(ModelViewSet):
     """
-    When rendering viewset in the Browsable API use action's docstring
-    instead of one that belongs to class if possible.
-    (only for list and retrieve actions)
-    """
-    # FIXME:
-    # A race condition happen with OPTIONS that gives you a random
-    # docstring I'll check later to see if there is a better solution
-    # or just remove this from this code...
-    def __new__(cls, name, bases, namespace, **kwds):
-        classdoc = namespace['__doc__'] = namespace.get('__doc__', '')
+    Customised Django REST Framework's ModelViewSet:
 
-        def wrap(action):
-            if type(action) is not FunctionType:
-                return action
-
-            @wraps(action)
-            def _(obj, *args, **kwargs):
-                obj.__class__.__doc__ = getattr(action, '__doc__', classdoc)
-                return action(obj, *args, **kwargs)
-
-            return _
-
-        # FIXME: make configurable
-        for action in ['list', 'retrieve', 'my', 'invites']:
-            if action in namespace:
-                namespace[action] = wrap(namespace[action])
-
-        return type.__new__(cls, name, bases, namespace)
-
-
-class AppViewSet(ModelViewSet, metaclass=ActionDocViewsetMeta):
-    """
-    Django REST Framework's ModelViewSet on steroids:
-
-    a). You can specify different serializers for different actions
-        like this:
+    Can specify different serializers for different actions like this:
 
         MyViewSet(AppViewSet):
             serializer_classes = {
-               'list': MyListSerializer,
-               'create': MyCreateSerializer,
+               'list': MyListSerializer,      # list action serializer
+               'create': MyCreateSerializer,  # create action serializer
                ...
             }
-
-    b). Switch action method docstring with a class docstring for
-        rendering documentation in the Browsable API
+            serializer_class = MyOtherSerializer  # the default serializer
     """
     def get_serializer_class(self):
         try:
